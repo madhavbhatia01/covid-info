ca2e5746fd621969b52644846f72d05b6a2f7d4f
Madhav Bhatia <madhav@Madhavs-MacBook-Pro.local>
2021-06-16 21:21:50 +0530

    Deploying-1

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Started");
+});
